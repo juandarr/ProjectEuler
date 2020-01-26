4e003c6a051a83e1974749a2b7a9adebbc63f22a
Juan David Rios <juandarr@gmail.com>
2020-01-26 16:39:13 -0500

    add: solution to problem 31 of project euler; refactor: improved solution to problem 32, the number of operation is reduced

---
+++ 31.py
@@ -0,0 +1,34 @@
+"""
+Finds all possible combinations of coins to generate value
+Author: Juan Rios
+"""
+import math
+
+"""
+Calculate the possible combinations of coint to equate value
+"""
+def find_combs(coins,value, index):
+    if value%coins[index]==0 and index==0:
+        return 1
+    else:
+        max_counter = value//coins[index]
+        combs = 0
+        if value%coins[index]==0:
+            combs += 1
+            max_counter -= 1
+        for i in range(max_counter, -1, -1):
+            tmp_value = value - i*coins[index]
+            combs += find_combs(coins, tmp_value, index-1)
+        return combs
+
+
+
+
+if __name__ == "__main__":
+    value = 200
+    coins = [1,2,5,10,20,50,100,200]
+    for i in range(len(coins)-1,-1,-1):
+        if (coins[i]<=value):
+            index = i
+            break
+    print('The total possible combination to generate the value {0} is {1}'.format(value, find_combs(coins,value, index))) 
--- 32.py
+++ 32.py
@@ -25,35 +25,40 @@ def n_perm_k(n,k):
 """
 Calculate the sum of pandigital numbers
 """
-def total_pandigital():
+def total_pandigital_less():
     perm_1 = n_perm_k(9,1)
     perm_2 = n_perm_k(9,2)
+    
     perm_3 = n_perm_k(9,3)
+    perm_3_index = [0]*(int(perm_3[-1])+1)
+    for i in perm_3:
+        perm_3_index[int(i)]=1
+
     perm_4 = n_perm_k(9,4)
-    total = 0
-    vals = {}
-    for num in perm_4:
-        for div in perm_2:
-            val = int(num)
-            div_val = int(div)
-            if val%div_val==0:
-                if str(val//div_val) in perm_3:
-                    if set(str(val//div_val)+num+div)=={'1','2','3','4','5','6','7','8','9'}:
-                        if val not in vals:
-                            print((val//div_val), div, val)
-                            total += val
-                            vals[val] = 1
-        for div in perm_1:
-            val = int(num)
-            div_val = int(div)
-            if val%div_val==0:
-                if str(val//div_val) in perm_4:
-                    if set(str(val//div_val)+num+div)=={'1','2','3','4','5','6','7','8','9'}:
-                        if val not in vals:
-                            print((val//div_val), div, val)
-                            total += val
-                            vals[val] = 1
-    return total
+    perm_4_index = [0]*(int(perm_4[-1])+1)
+    for i in perm_4:
+        perm_4_index[int(i)]=1
+
+    total = set()
+    operations = 0
+    for num1 in perm_3:
+        for num2 in perm_2:
+            operations += 1
+            prod = int(num1)*int(num2)
+            if prod < len(perm_4_index):
+                if perm_4_index[prod]==1:
+                    if set(num1+num2+str(prod))=={'1','2','3','4','5','6','7','8','9'}:
+                                total.add(prod)
+    for num1 in perm_4:
+        for num2 in perm_1:
+            operations += 1
+            prod = int(num1)*int(num2)
+            if prod < len(perm_4_index):
+                if perm_4_index[prod]==1:
+                    if set(num1+num2+str(prod))=={'1','2','3','4','5','6','7','8','9'}:
+                                total.add(prod)
+        
+    return operations ,sum(total)
 
 if __name__ == "__main__":
-    print('The sum of all the products is {0}'.format(total_pandigital())) 
+    print('The sum of all the products is {0}'.format(total_pandigital_less())) 
