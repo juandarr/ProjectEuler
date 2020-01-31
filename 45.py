c0e93715e2fbaa6641548059abb5a16e79bffa24
Juan David Rios <juandarr@gmail.com>
2020-01-31 12:48:40 -0500

    add: solution to problem 45 of project euler

---
+++ 45.py
@@ -0,0 +1,23 @@
+"""
+Finds the next triangle number after T285=P165=H143 that is also pentagonal and hexagonal
+Author: Juan Rios
+"""
+
+import math
+
+"""
+Return next number that is hexagonal, pentagonal and triangle
+"""
+def geometric_number(starting_index):
+    c = starting_index
+    while True:
+        num = c*(2*c-1)
+        a = -0.5+math.sqrt(0.5**2+2*num)
+        if a == int(a):
+            b = (1+math.sqrt(1+24*num))/6
+            if b == int(b):
+                return num
+        c += 1
+
+if __name__ == "__main__":
+    print('The next number after T285 that is pentagonal and hexagonal is is {0}'.format(geometric_number(144))) 
