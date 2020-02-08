c61a0a06ae809cf1848ec77c95cda2820808156f
Juan David Rios <juandarr@gmail.com>
2020-02-07 22:06:29 -0500

    feat: completed problem 60, the hardest so far of project euler

--- 60.py
+++ 60.py
@@ -6,69 +6,83 @@ Author: Juan Rios
 import math
 from utils import prime_factors
 
+lower_boundary = 26033
+primes = prime_factors(lower_boundary)
+primes_index = prime_factors(10**7, False)
+
 """
 Returns 1 if the number is prime, 0 otherwise
 """
-def is_prime(number, primes):
+def is_prime(number):
+    if number < len(primes_index):
+        return primes_index[number]
     for p in primes:
         if p > math.sqrt(number):
             return 1
         if number%p==0:
             return 0
 
+def reduce_candidates(candidates):
+    pivot = candidates[0]
+    tmp =[]
+    for p in candidates[1:]:
+        n1 = int(str(p)+str(pivot))
+        n2 = int(str(pivot)+str(p))
+        if is_prime(n1) and is_prime(n2):
+            tmp.append(p)
+    return tmp
+
 """
 Returns the number lowest sum of set of five primes
 """
 def lowest_prime_sum():
-    #[3, 41, 719, 12413, 634679] 647855
-    #[3, 349, 373, 1237, 240733] 242695
-    primes = prime_factors(242695)
-    primes_index = prime_factors(10**7, False)
-    #remarkable_primes = [3,7,109,673]
-
-    starting_index = 4
-    completed = False
-    max_sum = float('inf')
-
-    while not(completed):
-        index= starting_index
-        remarkable_primes = [3,primes[index-1]]
-        mod = remarkable_primes[1]%3
-        while (len(remarkable_primes)<5):
-        
-            while (index < len(primes)):
-                if (primes[index]%3!=mod):
-                    index += 1
-                    continue
-                tmp = remarkable_primes + [primes[index]]
-                all_prime = True
-                for elem in tmp[:-1]:
-                    n1 = int(str(elem)+str(tmp[-1]))
-                    n2 = int(str(tmp[-1])+str(elem))
-                    if n1<10**7 and n2<10**7:
-                        if primes_index[n1]==0 or primes_index[n2]==0:
-                            all_prime = False
-                            break
-                    else: 
-                        if is_prime(n1, primes)==0 or is_prime(n2, primes)==0:
-                            all_prime = False
-                            break
-                if all_prime:
-                    remarkable_primes.append(primes[index])
-                    #print(remarkable_primes)
-                    index += 1
-                    break
-                index += 1
-            if (index >= len(primes)):
-                starting_index += 1
+    min_sum = float('inf')
+    for idx0 in range(2, len(primes)):
+        prime_candidates0 = reduce_candidates(primes[idx0:])
+        mod = primes[idx0]%3
+        print(idx0)
+        if primes[idx0]> lower_boundary/5:
+            break
+        for idx1 in range(len(prime_candidates0)):
+            if prime_candidates0[idx1]%3 != mod:
+                continue
+            ar = [primes[idx0], prime_candidates0[idx1]]
+            if sum(ar)>min_sum:
                 break
-        if (len(remarkable_primes)==5):
-            if max_sum > sum(remarkable_primes):
-                max_sum = sum(remarkable_primes)
-                print(remarkable_primes, max_sum,index)
-                starting_index += 1
-                #completed = True
-    return max_sum
+            prime_candidates1 = reduce_candidates(prime_candidates0[idx1:])
+            for idx2 in range(len(prime_candidates1)):
+                if prime_candidates1[idx2]%3 != mod:
+                    continue
+                ar = [primes[idx0], prime_candidates0[idx1],prime_candidates1[idx2]]
+                if sum(ar)>min_sum:
+                    break
+                prime_candidates2 =  reduce_candidates(prime_candidates1[idx2:])
+                for idx3 in range(len(prime_candidates2)):
+                    if prime_candidates2[idx3]%3 != mod:
+                        continue
+                    ar = [primes[idx0], prime_candidates0[idx1],prime_candidates1[idx2],prime_candidates2[idx3]]
+                    if sum(ar)>min_sum:
+                        break
+                    prime_candidates3 =  reduce_candidates(prime_candidates2[idx3:])
+                    for idx4 in range(len(prime_candidates3)):
+                        if prime_candidates3[idx4]%3!=mod:
+                            continue
+                        ar = [primes[idx0], prime_candidates0[idx1],prime_candidates1[idx2],prime_candidates2[idx3],prime_candidates3[idx4]]
+                        if min_sum > sum(ar):
+                            min_sum = sum(ar)
+                            print(ar, sum(ar))
+                        else:
+                            break
+    return min_sum
+                
+
+    min_sum = float('inf')
+
+    
+
+            
+
         
+
 if __name__ == "__main__":
     print('The lowest sum of a set of five primes is {0}'.format(lowest_prime_sum()))
