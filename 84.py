e94e05960f46684b1b99fc81205735d58c457ba1
Juan David Rios <juandarr@gmail.com>
2020-02-24 22:11:32 -0500

    add: solution to problem 84

--- 84.py
+++ 84.py
@@ -3,181 +3,105 @@ Finds the odds in the monopoly game for two 4 sided dices
 Author: Juan Rios
 """
 import math
+import numpy as np
+import random 
+#random.seed(a=4)
 
-"""
-Calculates the number of rectangles in a l*h grid
-"""
-def dices(sides):
-    dice_odds = {}
-    for i in range(1,sides+1):
-        for j in range(1,sides+1):
-            if i+j in dice_odds:
-                dice_odds[i+j]+= 1
+class rules_deck():
+    def __init__(self):
+        self.cc = [0,10]
+        random.shuffle(self.cc)
+        self.ch = [0,10,11,24,39,5]
+        random.shuffle(self.ch)
+        self.cc_counter = 0
+        self.ch_counter = 0
+
+    def apply_rule_and_move(self, current_cell):
+        next_cell = current_cell
+
+        if next_cell in [7,22,36]:
+            self.ch_counter %= 16
+            if self.ch_counter<len(self.ch):
+                next_cell = self.ch[self.ch_counter]
+            elif self.ch_counter in [6,7]:
+                if current_cell==7:
+                    next_r = 15
+                elif current_cell==22:
+                    next_r = 25
+                elif current_cell==36:
+                    next_r = 5
+                next_cell = next_r
+            elif self.ch_counter==8:
+                if current_cell==7:
+                    next_u = 12 
+                elif current_cell==22:
+                    next_u = 28
+                elif current_cell==36:
+                    next_u = 12
+                next_cell = next_u
+            elif self.ch_counter==9:
+                next_cell = current_cell-3
             else:
-                dice_odds[i+j] = 1
-    return dice_odds
+                next_cell = current_cell
+            self.ch_counter += 1
 
-def monopoly_rules(current_cell,remove= False, elements_to_remove=[]):
-    if remove==False:
-        total = 16
-    else:
-        total = 16 - len(elements_to_remove)
+        if next_cell in [2,17,33]:
+            self.cc_counter %= 16
+            if self.cc_counter<len(self.cc):
+                next_cell = self.cc[self.cc_counter]
+            else:
+                next_cell = current_cell
+            self.cc_counter +=1
+            
+        
+        if next_cell==30:
+            next_cell = 10
+        return next_cell
 
-    if current_cell in [2,17,33]:
-        ar = [[1/total,10],[1/total,0],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell]]
-        if remove==False:
-            return ar
-        else:
-            tmp = []
-            for i in range(len(ar)):
-                if i not in elements_to_remove:
-                    tmp.append(ar[i])
-            return tmp
-    elif current_cell in [7,22,36]:
-        if current_cell==7:
-            next_u = 12 
-            next_r = 15
-        elif current_cell==22:
-            next_u = 28
-            next_r = 25
-        elif current_cell==36:
-            next_u = 12
-            next_r = 5
-        ar = [[1/total,0],[1/total,10],[1/total,11],[1/total,24],[1/total,39],[1/total,5],[1/total,next_r],[1/total,next_r],[1/total,next_u],[1/total,current_cell-3],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell],[1/total,current_cell]]
-        if remove==False:
-            return ar
-        else:
-            tmp = []
-            for i in range(len(ar)):
-                if i not in elements_to_remove:
-                    tmp.append(ar[i])
-            return tmp
-    elif current_cell==30:
-        return [[None,10]]
-    else:
-        return [[None,current_cell]]
+class monopoly_game():
 
-def monopoly_cell_odds(sides):
-    monopoly_odds = {}
-    dice_odds = dices(sides)
-    total = sides*sides
-    for key in dice_odds:
-        current_cell = 0
-        current_cell += key
-        options = monopoly_rules(current_cell)
-        if key%2==1:
-            for opt in options:
-                prob = dice_odds[key]/total
-                if opt[0]==None:
-                    if opt[1] in monopoly_odds:
-                        monopoly_odds[opt[1]] += prob
-                    else:
-                        monopoly_odds[opt[1]] = prob
-                else:
-                    if opt[1] in monopoly_odds:
-                        monopoly_odds[opt[1]] += prob*opt[0]
-                    else:
-                        monopoly_odds[opt[1]] = prob*opt[0]
+    def __init__(self,dice_sides):
+        self.doubles = 0
+        self.previous_double = False
+        self.sides = dice_sides
+        self.rules = rules_deck()
+        self.outcomes = {}
+        for i in range(40):
+            self.outcomes[i]=0
+
+    def roll_dices(self):
+        dice1 = random.randint(1,self.sides)
+        dice2 = random.randint(1,self.sides)
+        if dice1==dice2 and self.doubles == 0:
+            self.doubles = 1
+            self.previous_double = True
+        elif dice1==dice2 and self.previous_double:
+            self.doubles += 1
         else:
-            prob = (dice_odds[key]-1)/total
-            for opt in options:
-                index0 = options.index(opt)
-                if opt[0]==None:
-                    if opt[1] in monopoly_odds:
-                        monopoly_odds[opt[1]] += prob
-                    else:
-                        monopoly_odds[opt[1]] = prob
-                else:
-                    if opt[1] in monopoly_odds:
-                        monopoly_odds[opt[1]] += prob*opt[0]
-                    else:
-                        monopoly_odds[opt[1]] = prob*opt[0]
-                for key in dice_odds:
-                    tmp_cell = opt[1]
-                    tmp_cell += key
-                    tmp_cell %=40
-                    if current_cell in [7,22,36] and tmp_cell in [7,22,36]:
-                        tmp_options = monopoly_rules(tmp_cell, True, [index0])
-                    elif current_cell in [2,17,33] and tmp_cell in [2,17,33]:
-                        tmp_options = monopoly_rules(tmp_cell, True, [index0])
-                    else:
-                        tmp_options = monopoly_rules(tmp_cell)
-                    if key%2==1:
-                        for tmp_opt in tmp_options:
-                            tmp_prob = (1/total)*(dice_odds[key]/total)
-                            if tmp_opt[0]==None:
-                                if tmp_opt[1] in monopoly_odds:
-                                    monopoly_odds[tmp_opt[1]] += tmp_prob
-                                else:
-                                    monopoly_odds[tmp_opt[1]] = tmp_prob
-                            else:
-                                if tmp_opt[1] in monopoly_odds:
-                                    monopoly_odds[tmp_opt[1]] += tmp_prob*tmp_opt[0]
-                                else:
-                                    monopoly_odds[tmp_opt[1]] = tmp_prob*tmp_opt[0]
-                    else:
-                        for tmp_opt in tmp_options:
-                            index1 = tmp_options.index(tmp_opt)
-                            tmp_prob = (1/total)*((dice_odds[key]-1)/total)
-                            if tmp_opt[0]==None:
-                                if tmp_opt[1] in monopoly_odds:
-                                    monopoly_odds[tmp_opt[1]] += tmp_prob
-                                else:
-                                    monopoly_odds[tmp_opt[1]] = tmp_prob
-                            else:
-                                if tmp_opt[1] in monopoly_odds:
-                                    monopoly_odds[tmp_opt[1]] += tmp_prob*tmp_opt[0]
-                                else:
-                                    monopoly_odds[tmp_opt[1]] = tmp_prob*tmp_opt[0]
-                            for key in dice_odds:
-                                tmp2_cell = tmp_opt[1]
-                                tmp2_cell += key
-                                tmp2_cell %=40
-                                if tmp_cell in [7,22,36] and tmp2_cell in [7,22,36]:
-                                    tmp_index = index1
-                                    if index1 >= index0:
-                                        tmp_index = index1+1
-                                    tmp2_options = monopoly_rules(tmp2_cell, True, [index0,tmp_index])
-                                elif tmp_cell in [2,17,33] and tmp2_cell in [2,17,33]:
-                                    tmp_index = index1
-                                    if index1 >= index0:
-                                        tmp_index = index1+1
-                                    tmp2_options = monopoly_rules(tmp2_cell, True, [index0,tmp_index])
-                                else:
-                                    tmp2_options = monopoly_rules(tmp2_cell)
-                                if key%2==1:
-                                    for tmp2_opt in tmp2_options:
-                                        tmp2_prob = ((1/total)**2)*(dice_odds[key]/total)
-                                        if tmp2_opt[0]==None:
-                                            if tmp2_opt[1] in monopoly_odds:
-                                                monopoly_odds[tmp2_opt[1]] += tmp2_prob
-                                            else:
-                                                monopoly_odds[tmp2_opt[1]] = tmp2_prob
-                                        else:
-                                            if tmp2_opt[1] in monopoly_odds:
-                                                monopoly_odds[tmp2_opt[1]] += tmp2_prob*tmp2_opt[0]
-                                            else:
-                                                monopoly_odds[tmp2_opt[1]] = tmp2_prob*tmp2_opt[0]
-                                else:
-                                    tmp2_prob = ((1/total)**2)*((dice_odds[key]-1)/total)
-                                    for tmp2_opt in tmp2_options:
-                                        if tmp2_opt[0]==None:
-                                            if tmp2_opt[1] in monopoly_odds:
-                                                monopoly_odds[tmp2_opt[1]] += tmp2_prob
-                                            else:
-                                                monopoly_odds[tmp2_opt[1]] = tmp2_prob
-                                        else:
-                                            if tmp2_opt[1] in monopoly_odds:
-                                                monopoly_odds[tmp2_opt[1]] += tmp2_prob*tmp2_opt[0]
-                                            else:
-                                                monopoly_odds[tmp2_opt[1]] = tmp2_prob*tmp2_opt[0]
-                                        prob_jail =((1/total)**3)
-                                        if 10 in monopoly_odds:
-                                            monopoly_odds[10]+= prob_jail
-                                        else:
-                                            monopoly_odds[10] = prob_jail
-    return sorted(monopoly_odds.items() ,  key=lambda x: x[1], reverse=True)
+            self.doubles = 0
+            self.previous_double = False
+        return dice1+dice2
+
+    def one_step_game(self,current_position):
+        dice_move = self.roll_dices()
+        if self.doubles==3:
+            self.doubles = 0
+            self.previous_double = False
+            next_position = 10
+            self.outcomes[next_position]+=1
+        else:
+            next_position = self.rules.apply_rule_and_move(dice_move+current_position)%40
+            self.outcomes[next_position]+=1
+        return next_position
 
 if __name__ == "__main__":
-    sides = 6 
-    print('The odds of a number given two {0} sided dices is {1}'.format(sides,monopoly_cell_odds(sides)))
+    sides = 4
+    monopoly = monopoly_game(sides)
+    position = 0
+    for _ in range(10**6):
+        position = monopoly.one_step_game(position)
+    total = sum([i[1] for i in sorted(monopoly.outcomes.items() ,  key=lambda x: x[1], reverse=True)])
+    for key in monopoly.outcomes:
+        monopoly.outcomes[key] /= total
+    sol=sorted(monopoly.outcomes.items() ,  key=lambda x: x[1], reverse=True)
+    print('The odds of a number given two {0} sided dices is {1}'.format(sides,[i[0] for i in sol[:3] ]))
