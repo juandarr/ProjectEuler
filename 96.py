0055019d82250d1114c86923514cc8f68b22fa95
Juan David Rios <juandarr@gmail.com>
2020-03-04 12:24:53 -0500

    add: basic structure of solution to problem 96

---
+++ 96.py
@@ -0,0 +1,19 @@
+"""
+Solves fifty sudoku puzzles and adds 3-digit values from the top left corner
+Author: Juan Rios
+"""
+import math
+
+
+def read_sudoku(filename):
+    """
+    Read sudoku file
+    """
+    pass
+    
+def sudoku_solver(sudoku_list):
+    pass
+
+if __name__ == "__main__":
+    filename = ''
+    print('The sum of all 3-digit number of all solutions is {0}'.format(sudoku_solver(filename))) 
