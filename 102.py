5f9a5f416e21c9c6aca1a1b09f0c8d7fadb3d372
Juan David Rios <juandarr@gmail.com>
2020-03-08 02:25:21 -0500

    refactor: improved solution to problem 102

--- 102.py
+++ 102.py
@@ -27,42 +27,46 @@ def contains_origin_checker(filename):
         #0: x negative, 1: x positive, 2: y negative, 3: y positive
         intercept_axis=[False, False, False, False] 
         origin_contained = False
-        for side in combinations([0,1,2],2):
-            if t[side[0]][0]!=t[side[1]][0] and t[side[0]][1]!=t[side[1]][1]:
-                m = (t[side[0]][1]-t[side[1]][1])/(t[side[0]][0]-t[side[1]][0])
-                b = t[side[0]][1]-m*t[side[0]][0]
-                y_limits = sorted([t[side[0]][1],t[side[1]][1]])
-                x_limits = sorted([t[side[0]][0],t[side[1]][0]])
-                if b<=y_limits[1] and b>=y_limits[0]:
-                    if b > 0 and b<=y_limits[1] and b>=y_limits[0]:
-                        intercept_axis[3]=True
-                    elif b <0:
-                        intercept_axis[2]=True
-                x_i = -b/m
-                if x_i<=x_limits[1] and x_i>=x_limits[0]:
-                    if x_i > 0:
-                        intercept_axis[1]=True
-                    elif x_i <0:
-                        intercept_axis[0]=True
-            elif t[side[0]][1]==t[side[1]][1]:
-                b = t[side[0]][1]
-                if b<=y_limits[1] and b>=y_limits[0]:
-                    if b > 0:
-                        intercept_axis[3]=True
-                    elif b <0:
-                        intercept_axis[2]=True
-            elif t[side[0]][0]==t[side[1]][0]:
-                x_i = t[side[0]][0]
-                if x_i<=x_limits[1] and x_i>=x_limits[0]:
-                    if x_i > 0:
-                        intercept_axis[1]=True
-                    elif x_i <0:
-                        intercept_axis[0]=True
-            if intercept_axis==[True]*4:
-                origin_contained = True
-                break
-        if origin_contained:
-            total += 1
+        # Checks whether all points are at the same side of one axis, this means the origin is not included
+        if all([point[0]>0 for point in t]) or all([point[0]<0 for point in t]) or all([point[1]>0 for point in t]) or all([point[1]<0 for point in t]):
+            continue
+        else:
+            for side in combinations([0,1,2],2):
+                if t[side[0]][0]!=t[side[1]][0] and t[side[0]][1]!=t[side[1]][1]:
+                    m = (t[side[0]][1]-t[side[1]][1])/(t[side[0]][0]-t[side[1]][0])
+                    b = t[side[0]][1]-m*t[side[0]][0]
+                    y_limits = sorted([t[side[0]][1],t[side[1]][1]])
+                    x_limits = sorted([t[side[0]][0],t[side[1]][0]])
+                    if b<=y_limits[1] and b>=y_limits[0]:
+                        if b > 0 and b<=y_limits[1] and b>=y_limits[0]:
+                            intercept_axis[3]=True
+                        elif b <0:
+                            intercept_axis[2]=True
+                    x_i = -b/m
+                    if x_i<=x_limits[1] and x_i>=x_limits[0]:
+                        if x_i > 0:
+                            intercept_axis[1]=True
+                        elif x_i <0:
+                            intercept_axis[0]=True
+                elif t[side[0]][1]==t[side[1]][1]:
+                    b = t[side[0]][1]
+                    if b<=y_limits[1] and b>=y_limits[0]:
+                        if b > 0:
+                            intercept_axis[3]=True
+                        elif b <0:
+                            intercept_axis[2]=True
+                elif t[side[0]][0]==t[side[1]][0]:
+                    x_i = t[side[0]][0]
+                    if x_i<=x_limits[1] and x_i>=x_limits[0]:
+                        if x_i > 0:
+                            intercept_axis[1]=True
+                        elif x_i <0:
+                            intercept_axis[0]=True
+                if intercept_axis==[True]*4:
+                    origin_contained = True
+                    break
+            if origin_contained:
+                total += 1
     return total
     
 if __name__ == "__main__":
