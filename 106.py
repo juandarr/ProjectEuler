fe74f2f76f5a8bea33caaa879dbd0ec86a8a7b10
Juan David Rios <juandarr@gmail.com>
2020-03-11 15:36:51 -0500

    add: solution to problem 106 of project euler

--- 106.py
+++ 106.py
@@ -1,5 +1,5 @@
 """
-Finds the number of triangles containing the origin in its interior
+Finds the number of tests required to guarantee that a given n element set is a special set sum
 Author: Juan Rios
 """
 import math
@@ -20,11 +20,53 @@ def read_sets(filename):
         sets[idx] = sorted(points)
     return sets
 
+def is_matrix_unique(matrix):
+    for idx1 in range(len(matrix)):
+        candidates =[idx2 for idx2 in range(len(matrix)) if matrix[idx2][idx1]==1]
+        best = None
+        min_score = float('inf')
+        for idx2 in candidates:
+            if sum(matrix[idx2][idx1+1:])<min_score:
+                min_score = sum(matrix[idx2][idx1+1:])
+                best = idx2
+        if best!=None:
+            tmp = matrix[idx1]
+            matrix[idx1]=matrix[idx2]
+            matrix[idx2]=tmp
+    for idx in range(len(matrix)):
+        if matrix[idx][idx]==1:
+            continue
+        else:
+            return False
+    return True
+
+def check_uniqueness(a,b):
+    matrix_p =[]
+    matrix_m =[]
+    for i in a:
+        matrix_p.append([])
+        matrix_m.append([])
+        for j in b:
+            if i>j:
+                matrix_p[-1].append(1)
+            else:
+                matrix_p[-1].append(0)
+            if i<j:
+                matrix_m[-1].append(1)
+            else:
+                matrix_m[-1].append(0)
+    return is_matrix_unique(matrix_p) or is_matrix_unique(matrix_m)
+    
+
+
 def test_special_sets(filename):
     sets = read_sets(filename)
     total = 0
     for number_set in sets:
+        #c = math.factorial(len(number_set))//(math.factorial(len(number_set)-2)*2)
+        c = 0
         is_special = True
+        ops = {}
         for idx in range(len(number_set)-1):
             if number_set[idx]==number_set[idx+1]:
                 is_special = False
@@ -44,6 +86,12 @@ def test_special_sets(filename):
                         b = 0
                         for idx in comb2:
                             b += number_set[idx]
+                        
+                        if (a,b) not in ops and (b,a) not in ops:
+                            if len(comb)==len(comb2):
+                                if not check_uniqueness(comb,comb2):
+                                    c+=1
+                            ops[(a,b)]=1
                         if a==b:
                             is_special=False
                             break
@@ -61,11 +109,15 @@ def test_special_sets(filename):
                     break
             if not is_special:
                 break
-        print(number_set,len(number_set), is_special)
+        print(number_set,c)
         if is_special:
             total += sum(number_set)
     return total
 
 if __name__ == "__main__":
-    filename = 'files/sets.txt'
+    filename = 'files/sets_test.txt'
+    #m = [[[1,2],[3,4]],[[1,3],[2,4]],[[1,4],[2,3]]]
+    #for el in m:
+    #    print(el, check_uniqueness(el[0],el[1]))
+    #print(is_matrix_unique([[0,1,0,0],[0,1,1,0],[1,0,1,0],[0,1,0,0]]))
     print('The sum of elements of all special sets is {0}'.format(test_special_sets(filename))) 
