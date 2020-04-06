a5dcbfd70ee42805668a3095e3818eea5d379448
Juan David Rios <juandarr@gmail.com>
2020-04-06 13:19:15 -0500

    refactor: improved solution to problem 142 of project euler

--- 142.py
+++ 142.py
@@ -5,6 +5,9 @@ Author: Juan Rios
 
 import math
 from time import time
+from decimal import *
+from utils import prime_factors,divisorGen
+import numpy as np
 
 """
 Finds the smallest x+y+z with several boundaries defined as perfect squares
@@ -15,30 +18,28 @@ def bounded_perfect_squares(limit_n):
     for i in range(1, 10**5+1):
         squares_idx[i**2]=i
         squares.append(i**2)
-    for idx_k in range(len(squares)):
-        k = squares[idx_k]
-        mod_k = k%2
+    for k in squares:
         if k>limit_n:
             break
-        for idx_w in range(idx_k):
-            w = squares[idx_w]
-            if (w%2)!=mod_k:
-                continue
-            if w>limit_n:
+        mod_k = k%2
+        for j in squares:
+            if k<=(j+1):
                 break
-            for idx_n in range(idx_w):
-                n = squares[idx_n]
-                if n>limit_n:
-                    break
-                j = w - n
-                m = k - j
-                i = k + n 
-                if i in squares_idx and m in squares_idx and j in squares_idx:
-                    if (k-w)%2==0:
-                        z = 3*((k-w)//2)+w+n
-                        print(z,i,j,k,w,m,n)
-                        return z
+            m = k - j
+            if m in squares_idx:
+                for n in squares:
+                    w = n+j
+                    i = k+n
+                    if k<=w:
+                        break
+                    if (w%2)!=mod_k:
+                        continue
+                    if w in squares_idx and i in squares_idx:
+                        if (3*k+n-j)%2==0:
+                            number = (3*k+n-j)//2
+                            print(number,i,j,k,w,k-j,n)
+                            return number
 
 if __name__ == "__main__":
-    limit_n = 10**10
+    limit_n = 10**8
     print('The smallest x+y+z with several boundaries defined as perfect squares is {0}'.format(bounded_perfect_squares(limit_n)))
