df243d3b059200a896d0b86d630b5a1f298f6156
Juan David Rios <juandarr@gmail.com>
2020-04-09 01:47:07 -0500

    feat: completed solution to problem 144 of project euler

--- 144.py
+++ 144.py
@@ -6,17 +6,41 @@ Author: Juan Rios
 import math
 from time import time
 from decimal import *
-from utils import prime_factors,divisorGen
-import numpy as np
 from PIL import Image, ImageDraw
-from time import time
-from IPython.display import clear_output
+
+getcontext().prec =20
+
+def pi():
+    """Compute Pi to the current precision.
+
+    >>> print(pi())
+    3.141592653589793238462643383
+
+    """
+    getcontext().prec += 2  # extra digits for intermediate steps
+    three = Decimal(3)      # substitute "three=3.0" for regular floats
+    lasts, t, s, n, na, d, da = 0, three, 3, 1, 0, 0, 24
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    getcontext().prec -= 2
+    return +s               # unary plus applies the new precision
+
+# Create the frames
+'''
+frames = []
+x, y = 0, 0
+for i in range(10):
+    new_frame = create_image_with_ball(400, 400, x, y, 40)
+    frames.append(new_frame)
+    x += 40
+    y += 40
+'''
 
 
-im = Image.new('RGB', (200, 300), (255, 255, 255))
-draw = ImageDraw.Draw(im)
-
-draw.ellipse((50, 50, 150, 250), fill=(255, 255, 255), outline=(2, 2, 0))
 
 """
 Finds how many  how many  times a laser beam hits internally an eliptic mirror before leaving
@@ -24,76 +48,52 @@ Finds how many  how many  times a laser beam hits internally an eliptic mirror b
 def eliptic_mirror(x0,y0,x1,y1):
     contacts = 1
     limit =2
+    frames = []
+    im = Image.new('RGB', (400, 600), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
 
+    draw.ellipse((100, 100, 300, 500), fill=(255, 255, 255), outline=(2, 2, 0))
+    frames.append(im.copy())
     while True:
-        
-        draw.line((x0*10+100, -10*y0+150, 10*x1+100, -10*y1+150), fill=(255, 0, 0), width=1)
-        t0 = time()
-        if contacts==limit:  
-            im.show()
-            print(contacts)
-            limit+=1
-            while time()-t0<2:
-                continue
-    
-        # Calculate the reflection angle from the normal to the surface and the incidence beam
-        m_n = y1/(4*x1)
-        b = y1-m_n*x1
-        theta_n = math.atan(m_n)
-        a = (x0-x1,y0-y1)
-        norm_a = (a[0]**2+a[1]**2).sqrt()
-        n = (-5,m_n*(x1-5)+b-y1)
-        norm_n = (n[0]**2+n[1]**2).sqrt()
-        theta_i = math.acos((n[0]*a[0]+n[1]*a[1])/(norm_a*norm_n))
-        if (theta_i)*(180/math.pi)>90:
-            theta_i = -theta_i+math.pi
-            theta_r = theta_n+theta_i 
-        elif (theta_i)*(180/math.pi)<90:
-            theta_i = math.pi +theta_i
-            theta_r = theta_n+theta_i
-        #elif ((theta_n)*(180/math.pi)>0 and (theta_i)*(180/math.pi)>90) or ((theta_n)*(180/math.pi)>0 and (theta_i)*(180/math.pi)<90):
-        #    theta_r = theta_n-theta_i
-        else:
-            theta_r = theta_n+theta_i
+        draw.line((x0*20+200, -20*y0+300, 20*x1+200, -20*y1+300), fill=(255, 0, 0), width=1)
 
-        '''
-        if ((theta_n)*(180/math.pi)<0 and (theta_i)*(180/math.pi)>90) or ((theta_n)*(180/math.pi)<0 and (theta_i)*(180/math.pi)<90):
-            theta_r = math.pi+theta_n-theta_i
-        elif ((theta_n)*(180/math.pi)>0 and (theta_i)*(180/math.pi)>90) or ((theta_n)*(180/math.pi)>0 and (theta_i)*(180/math.pi)<90):
-            theta_r = theta_n-theta_i
-        else:
-            theta_r = theta_n+theta_i
-        '''
+        if contacts==limit:
+            frames.append(im.copy())
+            limit+=1
+        # Calculate the reflection angle from the normal to the surface and the incidence beam
+        m_s = -4*x1/y1
+        theta_s = Decimal(math.atan(m_s))
+
+        m_i = (y0-y1)/(x0-x1)
+        theta_i_ref = Decimal(math.atan(m_i))
+
+        theta_r  = pi()+2*theta_s-theta_i_ref
+
         m = Decimal(math.tan(theta_r))
-        b = y1-Decimal(m)*x1
-        print(theta_n,theta_i, theta_r,m,b)
+
+        b = y1-m*x1
         
-        m2= m**2
-        b2 =b**2
+        ms= m**2
+        bs = b**2
+        root = (4*ms*bs-4*(4+ms)*(bs-100)).sqrt()
+        x0_tmp = (-2*m*b+root)/(2*(4+ms))
+        x1_tmp = (-2*m*b-root)/(2*(4+ms))
         
-        x0_tmp = (-2*m*b+(4*m2*b2-4*(4+m2)*(b2-100)).sqrt())/(2*(4+m2))
-        x1_tmp = (-2*m*b-(4*m2*b2-4*(4+m2)*(b2-100)).sqrt())/(2*(4+m2))
-        y0_tmp = m*x0_tmp+b
-        y1_tmp = m*x1_tmp+b
-        print((round(x1,4),round(y1,4)),(round(x0_tmp,4),round(y0_tmp,4)),(round(x1_tmp,4),round(y1_tmp,4)))
         x0,y0 = (x1,y1)
-        if round(x1,5)==round(x0_tmp,5):
-            x1 = x1_tmp
-            y1 = y1_tmp
-        else:
+
+        if abs(x1-x0_tmp)>=10**(-15):
             x1 = x0_tmp
-            y1 = y0_tmp
-        if x1>=-0.01 and x1<= 0.01:
+        else:
+            x1 = x1_tmp
+        y1 = m*x1+b
+
+        if abs(x1)<=0.01 and y1>0:
+            im.show()
+            # Save into a GIF file that loops forever
+            frames[0].save('laser_beam.gif', format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
             return contacts
         contacts+=1
-        print('Contact {0}, New points:'.format(contacts),(round(x0,4),round(y0,4)),(round(x1,4),round(y1,4)))
-        
-        
-        
-# Wrong: 161
-# Wrong: 274
-# Wrong: 275
-# Wrong: 162
+        #print('Contact {0}, New points:'.format(contacts),(round(x0,4),round(y0,4)),(round(x1,4),round(y1,4)))
 
 if __name__ == "__main__":
     print('The amount of times a laser beam hits internally an eliptic mirror before leaving is {0}'.format(eliptic_mirror(Decimal(0), Decimal(10.1),Decimal(1.4),Decimal(-9.6))))
