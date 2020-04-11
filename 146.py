0146baf86dedb1cbeba261860a82d137dd09143b
Juan David Rios <juandarr@gmail.com>
2020-04-11 18:38:57 -0500

    refactor: improves code with more efficient algorithm

--- 146.py
+++ 146.py
@@ -11,18 +11,20 @@ from utils import prime_factors
 def n_generator(start, limit_n):
     i = start
     while i<limit_n:
-        i+=2
+        i+=10
         if i%3==0 or i%7==0 or i%13==0:
             continue
         yield i
-        
+
 def are_consecutive_primes(num, primes):
     limit = int(math.sqrt(num+27))
+    values =[1,3,7,9,13,27]
     for p in primes:
         if p>limit:
             return True
-        elif (num+1)%p==0 or (num+1)%p==0 or (num+3)%p==0 or (num+7)%p==0 or (num+9)%p==0 or (num+13)%p==0 or (num+27)%p==0:
-            return False
+        for i in values:
+            if (num+i)%p==0:
+                return False
 
 def check_others_primes(num, primes):
     limit = int(math.sqrt(num+27))
@@ -32,10 +34,16 @@ def check_others_primes(num, primes):
         if p>limit:
             s = sum([1 for t in primality if t==True])
             return s
+        to_remove = []
         for i in values:
             if (num+i+1)%p==0:
+                to_remove.append(i)
                 primality[i]=False
-
+        for rem in to_remove:
+            values.remove(rem)
+        if values==[]:
+            return 6
+            
 """
 Finds consecutive primes pattern for values n below limit
 """
@@ -44,7 +52,7 @@ def consecutive_primes(limit_n):
     print('Primes calculated!')
     total = 0
     t0 = time()
-    for n in n_generator(2,limit_n):
+    for n in n_generator(10,limit_n):
         n2 = n**2
         if are_consecutive_primes(n2,primes):
             print('{0} is candidate!'.format(n))
