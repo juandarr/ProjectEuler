d4b269dd022f432f3c4052b43df7edf9de3f2066
Juan David Rios <juandarr@gmail.com>
2020-04-22 18:08:22 -0500

    refactor: cleaned code for better readability

--- 151.py
+++ 151.py
@@ -13,12 +13,9 @@ Finds the expected number of times the foreman finds a single sheet of paper in 
 def expected_value():
     day = 2
     paths = [[[1,1,1,1],1]]
-    prob = 0
     while day < 15:
         day+=1
-        counter_unit = 0
         tmp_ar = []
-        k = 0
         for path in paths:
             for idx in range(len(path[-2])):
                 if path[-2][idx]>0:
@@ -26,20 +23,16 @@ def expected_value():
                     tmp[idx]-=1
                     for idx2 in range(idx+1,len(path[-2])):
                         tmp[idx2]+=1
-                    if sum(tmp)==1:
-                        k+=1
                     tmp_prob = path[-1]*path[-2][idx]/sum(path[-2])
                     tmp_ar.append(path[:-1]+[tmp]+[tmp_prob])
         paths =tmp_ar
+    
     freq = {}
- 
     for path in paths:
-        c = 2
         k = 0
         for opt in path[:-1]:
             if sum(opt)==1:
                 k+=1
-            c+=1
         if k not in freq:
             freq[k]=path[-1]
         else:
